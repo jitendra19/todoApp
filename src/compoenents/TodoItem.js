94e19afd10469484a5658b9587995d9fc5c3c614
PriyankaAgAethereus <109947476+PriyankaAgAethereus@users.noreply.github.com>
2025-05-16 23:15:09 +0530

    delete button toggling

--- src/compoenents/TodoItem.js
+++ src/compoenents/TodoItem.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoItem({ item, deleteTask, onComplete }) {
-    return (<div key={item.time} className="todo-item">
+    const [showDelete, setShowDelete] = useState(false);
+    return (<div key={item.time} className="todo-item" onMouseEnter={(e) => {
+            setShowDelete(true);
+        }} onMouseLeave={()=> {
+            setShowDelete(false);
+        }}>
         <input type="checkbox" 
             onChange={() => onComplete(item.time)} 
             checked={item.completed}/>
         <span style={{ textDecoration: item.completed ? 'line-through' : 'none' }}> {item.text}</span>
-        <button onClick={() => {
+        <button style = {{visibility: showDelete ? "visible" : "hidden"}} onClick={() => {
             deleteTask(item.time);
         }}>Delete</button>
         <button onClick={() => {
